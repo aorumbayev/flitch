4ddab694786a0214799c2f394473e0f839cb53d8
Altynbek Orumbayev <aorumbayev@protonmail.com>
2020-02-23 20:30:27 +0100

    📖 preparing gh-pages

--- flitch/__init__.py
+++
@@ -1,5 +0,0 @@
-"""A collection of simple file splitting tools"""
-__version__ = "0.1.2"
-
-# Local
-from .splitter import split_into_parts
--- tests/test_flitch.py
+++
@@ -1,8 +0,0 @@
-# Local
-from flitch import __version__
-
-
-def test_version():
-    """Tests version of package
-    """
-    assert __version__ == "0.1.2"
