0bbf1311e4d73caf6559124e4aff0c62c976c543
Altynbek Orumbayev <aorumbayev@protonmail.com>
2020-02-23 21:00:52 +0100

    📦 Release 0.1.3

--- flitch/__init__.py
+++ flitch/__init__.py
@@ -1,5 +1,5 @@
 """A collection of simple file splitting tools"""
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 # Local
 from .splitter import split_into_parts
--- tests/test_flitch.py
+++ tests/test_flitch.py
@@ -5,4 +5,4 @@ from flitch import __version__
 def test_version():
     """Tests version of package
     """
-    assert __version__ == "0.1.2"
+    assert __version__ == "0.1.3"
